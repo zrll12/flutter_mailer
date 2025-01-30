fa2aa98b1d54cd1f7499818001788c529d10e07c
zrll_ <46812903+zrll12@users.noreply.github.com>
2025-01-30 22:46:35 +0800

    feat: add home screen

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<NavigationDestination> bottomNavigationBarItems = [
+    NavigationDestination(
+      label: 'Home',
+      icon: Icon(Icons.email_outlined),
+    ),
+    NavigationDestination(
+      label: 'Profile',
+      icon: Icon(Icons.person_outline),
+    ),
+    NavigationDestination(
+      label: 'Settings',
+      icon: Icon(Icons.settings_outlined),
+    ),
+  ];
+
+  int _bottomIndex = 0;
+
+  void _bottomIndexChanged(int index) {
+    setState(() {
+      _bottomIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Mailer'),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: bottomNavigationBarItems,
+        selectedIndex: _bottomIndex,
+        onDestinationSelected: _bottomIndexChanged,
+      ),
+    );
+  }
+}
