91a2891e04d6241bd068d8b86977de2f1e2f1dca
zrll_ <46812903+zrll12@users.noreply.github.com>
2025-03-10 16:20:21 +0800

    feat: add dynamic color

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mailer/page/profile/add_profile.dart';
-import 'package:flutter_mailer/screen/home_screen.dart';
 
 import 'page/email/email_details.dart';
+import 'page/profile/add_profile.dart';
 import 'page/profile/profile_details.dart';
+import 'screen/home_screen.dart';
 
 void main() {
   runApp(const FlutterMailer());
@@ -15,21 +16,46 @@ class FlutterMailer extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        brightness: Brightness.light,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      home: const HomeScreen(),
-      routes: {
-        'home': (context) => const HomeScreen(),
-        'add_profile': (context) => const AddProfilePage(),
-        'profile_details': (context) => ProfileDetails(),
-        'email_details': (context) => EmailDetails(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+
+        if (lightDynamic != null && darkDynamic != null) {
+          // On Android S+ devices, use the provided dynamic color scheme.
+          // (Recommended) Harmonize the dynamic color scheme' built-in semantic colors.
+          lightColorScheme = lightDynamic.harmonized();
+
+          // Repeat for the dark color scheme.
+          darkColorScheme = darkDynamic.harmonized();
+        } else {
+          // Otherwise, use fallback schemes.
+          lightColorScheme = ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+          );
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            brightness: Brightness.dark,
+          );
+        }
+
+        return MaterialApp(
+          title: "Flutter Mailer",
+          theme: ThemeData(
+            colorScheme: lightColorScheme,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme,
+          ),
+          home: const HomeScreen(),
+          routes: {
+            'home': (context) => const HomeScreen(),
+            'add_profile': (context) => const AddProfilePage(),
+            'profile_details': (context) => ProfileDetails(),
+            'email_details': (context) => EmailDetails(),
+          },
+          debugShowCheckedModeBanner: false,
+        );
       },
     );
   }
